b4c1f19928d37b67ae9e8de05803c85f4b5ba876
amar ali <amarhussain391@gmail.com>
2024-08-06 16:59:13 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -5,20 +5,26 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 
 const app = express();
-app.listen(3000, (req, res, next) => {
-  console.log("server is running");
-});
-var corsOptions = {
-  origin: "https://commerce-task-client.vercel.app/",
+
+const corsOptions = {
+  origin: "https://commerce-task-client.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  Credentials: true,
+  credentials: true,
 };
-app.use(cors());
-app.use(express.json(corsOptions));
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
 app.use("/api/auth", authRouter);
 app.use("/api/list", listingRouter);
+
 mongoose
   .connect(
     "mongodb+srv://amarhussain391:pFyBPpdxx3mGbfWr@cluster0.ifclsty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
-  .then(() => console.log("database connected"));
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.error("Database connection error:", error));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
